a99d2e4662c60f9cac781d5ef9aa5675ef5d2a3c
Leo Farias <leoafarias@gmail.com>
2023-08-29 09:21:36 -0400

    Update env var

--- test/constants.dart
+++ test/constants.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:dotenv/dotenv.dart';
 
+final isCI = Platform.environment['IS_CI'] == 'true';
+
 class TestEnv {
-  static final DotEnv _env = DotEnv(includePlatformEnvironment: true)..load();
+  static final DotEnv _env = DotEnv(includePlatformEnvironment: true);
   static get palmApiKey {
+    if (isCI) return Platform.environment['PALM_API_KEY'];
+    _env.load();
     return _env['PALM_API_KEY'];
   }
 }
