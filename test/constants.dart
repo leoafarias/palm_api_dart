12ab6f896b74c0d17683b077bd429b080e700e1f
Leo Farias <leoafarias@gmail.com>
2023-08-29 09:33:14 -0400

    Update workflow

--- test/constants.dart
+++ test/constants.dart
@@ -7,8 +7,6 @@ final isCI = Platform.environment['IS_CI'] == 'true';
 class TestEnv {
   static final DotEnv _env = DotEnv(includePlatformEnvironment: true);
   static get palmApiKey {
-    print('API KEY');
-    print(Platform.environment['PALM_API_KEY']);
     if (isCI) return Platform.environment['PALM_API_KEY'];
     _env.load();
     return _env['PALM_API_KEY'];
