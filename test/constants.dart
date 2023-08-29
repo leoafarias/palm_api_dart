168029d03a05d3d08019163c5e1bcb1b5d456a15
Leo Farias <leoafarias@gmail.com>
2023-08-29 09:31:20 -0400

    Print API KEY

--- test/constants.dart
+++ test/constants.dart
@@ -7,6 +7,8 @@ final isCI = Platform.environment['IS_CI'] == 'true';
 class TestEnv {
   static final DotEnv _env = DotEnv(includePlatformEnvironment: true);
   static get palmApiKey {
+    print('API KEY');
+    print(Platform.environment['PALM_API_KEY']);
     if (isCI) return Platform.environment['PALM_API_KEY'];
     _env.load();
     return _env['PALM_API_KEY'];
