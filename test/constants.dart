317b69b46a7c7c61cc0bd5b0bd05607955869640
Leo Farias <leoafarias@gmail.com>
2023-08-29 09:14:56 -0400

    Updated publish workflow

--- test/constants.dart
+++ test/constants.dart
@@ -1,7 +1,7 @@
 import 'package:dotenv/dotenv.dart';
 
 class TestEnv {
-  static final DotEnv _env = DotEnv()..load();
+  static final DotEnv _env = DotEnv(includePlatformEnvironment: true)..load();
   static get palmApiKey {
     return _env['PALM_API_KEY'];
   }
